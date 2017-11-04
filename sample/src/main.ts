2b23779d070a5cf6abef559844b3f3c5a1f5ccd2
pocketberserker <pocketberserker@gmail.com>
2017-11-04 14:34:15 +0900

    add multi column sample

--- sample/src/main.ts
+++ sample/src/main.ts
@@ -36,6 +36,32 @@ module.exports = function() {
       });
       pagination.content.append(rect);
     }
+
+    const multiColumn = new Pagination({
+      scene,
+      x: 100,
+      y: 0,
+      width: 150,
+      height: 150,
+      limit: {
+        vertical: 5,
+        horizontal: 5
+      },
+      position: Position.Bottom,
+      paddingRight: 10
+    });
+    scene.append(multiColumn);
+    for(let i = 0; i < 100; i++) {
+      const rect = new g.FilledRect({
+        scene,
+        cssColor: colors[g.game.random.get(0, 4)],
+        width: 10,
+        height: 10,
+        x: 10,
+        y: 10
+      });
+      multiColumn.content.append(rect);
+    }
   });
   g.game.pushScene(scene);
 }
